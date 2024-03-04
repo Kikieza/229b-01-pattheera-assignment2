75403919ceaca606e01821138334663054c35665
Kikieza <kikiepattheera@gmail.com>
2024-03-04 16:37:35 +0700

    WEEK9DONE

--- Assets/scripts/MoveMB.cs
+++ Assets/scripts/MoveMB.cs
@@ -4,27 +4,27 @@ using UnityEngine;
 
 public class MoveMB : MonoBehaviour
 {
-    
-    
+    public float force;
     void Start()
     {
         
     }
-    private void Update() 
+
+    private void Update()
     {
-MoveByKey();
+        MoveByKey();
     }
 
     public void MoveByKey()
     {
         Rigidbody rb = GetComponent <Rigidbody>();
         if (Input.GetKey(KeyCode.A))
-            rb.AddForce(Vector3.left);
+            rb.AddForce(Vector3.left * force);
         if (Input.GetKey(KeyCode.D))
-            rb.AddForce(Vector3.right);
+            rb.AddForce(Vector3.right* force);
         if (Input.GetKey(KeyCode.W))
-            rb.AddForce(Vector3.forward);
+            rb.AddForce(Vector3.forward* force);
         if (Input.GetKey(KeyCode.S))
-            rb.AddForce(Vector3.back);
+            rb.AddForce(Vector3.back* force);
     }
 }
